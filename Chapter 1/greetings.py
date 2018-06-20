67a8ef697a629146ff2f564d48ba1fffd3fa8bf5
Unknown <muratovic.samer@gmail.com>
2018-06-20 12:58:22 +0200

    Chapter 1 - Exercises

---
+++ Chapter 1/greetings.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 20 12:51:48 2018
+
+@author: Rasimsson
+"""
+
+names = ["James","Bruce","Wade","Peter"]
+greeting = "Greetings hero: "
+
+print(greeting + names[0])
+print(greeting + names[1])
+print(greeting + names[2])
+print(greeting + names[3])
---
+++ Chapter 1/names.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 20 12:50:11 2018
+
+@author: Rasimsson
+"""
+
+names = ["James","Bruce","Wade","Peter"]
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
