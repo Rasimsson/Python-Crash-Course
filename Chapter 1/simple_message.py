dc91978adb3427b180a373000074e1006871ca15
Unknown <muratovic.samer@gmail.com>
2018-06-20 11:49:38 +0200

    Chapter 1 - Exercises
    This will contain all exercises from Chapter 1 of the book Python Crash Course

---
+++ Chapter 1/simple_message.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 20 11:45:31 2018
+
+@author: Rasimsson
+"""
+message = "Hello World"
+print(message)
---
+++ Chapter 1/simple_messages.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 20 11:45:31 2018
+
+@author: Rasimsson
+"""
+message = "Hello World"
+print(message)
+
+message = "Changing this message now to this!"
+print(message)
